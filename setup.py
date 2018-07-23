522acd3a5567dd0ac66acea5ec2708d39932f906
Adrian Goins <mon@chus.cc>
2018-07-23 13:51:15 -0400

    update for modern setuptools

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-from distribute_setup import use_setuptools
-use_setuptools()
-
-#python setup.py sdist upload
 
 from setuptools import setup
 from stratum import version
@@ -14,7 +10,6 @@ setup(name='stratum',
       author_email='info@bitcion.cz',
       url='http://blog.bitcoin.cz/stratum',
       packages=['stratum',],
-      py_modules=['distribute_setup',],
       zip_safe=False,
       install_requires=['twisted', 'ecdsa', 'autobahn',]
      )
